02b3a7e8b3b3c0e550dfaa9a48fb7b4757fad6bf
Andrew Harris <andrew.harris@cyberark.com>
2024-03-20 13:22:15 -0400

    #15 - Implement data source for shared services

--- cyberark/provider.go
+++ cyberark/provider.go
@@ -118,7 +118,9 @@ func (p *cyberarkProvider) Configure(ctx context.Context, req provider.Configure
 
 // DataSources defines the data sources implemented in the provider.
 func (p *cyberarkProvider) DataSources(_ context.Context) []func() datasource.DataSource {
-	return nil
+	return []func() datasource.DataSource {
+		NewTokenDataSource,
+	  }
 }
 
 // Resources defines the resources implemented in the provider.
